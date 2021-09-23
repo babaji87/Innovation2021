af5fb3590b04942b382a0a74f22db3aafa6f7aff
anjnakS <66026895+anjnakS@users.noreply.github.com>
2021-09-23 11:52:29 -0600

    Update setupResourcesDay2.sh

--- setupResourcesDay2.sh
+++ setupResourcesDay2.sh
@@ -104,7 +104,7 @@ echo -e "spring.jms.servicebus.connection-string="$SBCONNSTRING
 egrep -lRZ "3243dffc-1cc3-42e6-89ad-9d7eeed1d23b" | xargs -0 -l sed -i -e s/"3243dffc-1cc3-42e6-89ad-9d7eeed1d23b"/$SP_APPID/g
 egrep -lRZ "S~GBcRDBuddGAFUPlj1Oj7YwH03drJ.0Ld" | xargs -0 -l sed -i -e s/"S~GBcRDBuddGAFUPlj1Oj7YwH03drJ.0Ld"/$SP_PASSWORD/g
 egrep -lRZ "https://contosokvakm.vault.azure.net/" | xargs -0 -l sed -i -e s+"https://contosokvakm.vault.azure.net/"+"https://"$KEYVAULTNAME".vault.azure.net/"+g
-egrep -lRZ $SC_OLDCONNECTION | xargs -0 -l sed -i -e 's+$SC_OLDCONNECTION+$SBCONNSTRING+g'
+egrep -lRZ $SC_OLDCONNECTION | xargs -0 -l sed -i -e "s+$SC_OLDCONNECTION+$SBCONNSTRING+g"
 egrep -lRZ $CLIENT_ORIG_IP | xargs -0 -l sed -i -e s/$CLIENT_ORIG_IP/$CLIENT_NEW_IP/g
 egrep -lRZ $SERVER_ORIG_IP | xargs -0 -l sed -i -e s/$SERVER_ORIG_IP/$SERVER_NEW_IP/gegrep -lRZ $POSTGRES_ORIG_USER | xargs -0 -l sed -i -e s/$POSTGRES_ORIG_USER/$/g
 egrep -lRZ $POSTGRES_ORIG_PASSWD | xargs -0 -l sed -i -e s/$POSTGRES_ORIG_PASSWD/$POSTGRES_NEW_PASSWD/g
