51afa1fca2d32d86b8119086313eec9b228a8045
anjnakS <66026895+anjnakS@users.noreply.github.com>
2021-09-30 08:32:53 -0600

    Create MaxStocksDataRepository.java

---
+++ Step5/stocks-worker/src/main/java/com/example/worker/MaxStocksDataRepository.java
@@ -0,0 +1,10 @@
+package com.example.worker;
+
+import com.example.worker.model.MaxStockData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaxStocksDataRepository extends JpaRepository<MaxStockData, Long> {
+
+}
